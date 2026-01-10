2b422a18753ff1a829e4c55d67d8681002d0cdc7
aayush <aayushd711@gmail.com>
2026-01-10 13:30:39 +0530

    note Details Page UI added

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -9,7 +9,7 @@ export const NoteCard = ({ note, setNotes }) => {
     const handleDelete = async (e, id) => {
         e.preventDefault();
 
-        if (!window.confirm("Are you sure you want to delete this note?")) return;
+        if (!window.confirm("Are you sure you want to delete this record?")) return;
 
         try {
             await api.delete(`/notes/${id}`);
